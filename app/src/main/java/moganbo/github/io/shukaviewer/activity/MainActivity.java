e294cc8d85d6cc52feea9378e706cad3d685e040
moganbo <circus.key.august@gmail.com>
2018-08-18 21:23:57 +0900

    initial commit

---
+++ app/src/main/java/moganbo/github/io/shukaviewer/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package moganbo.github.io.shukaviewer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+import moganbo.github.io.shukaviewer.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
